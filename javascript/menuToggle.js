0d9a9c4cc08b9779585c9f79f9be4caf3473f8fc
kiettran <tkiet7697@gmail.com>
2025-08-22 20:31:47 +0700

    done lading, des

--- javascript/menuToggle.js
+++ javascript/menuToggle.js
@@ -1,5 +1,5 @@
 const hamburger = document.querySelector('.hamburger-menu');
-  const mobileMenu = document.querySelector('.mobile-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
   hamburger.addEventListener('click', () => {
     mobileMenu.classList.toggle('active');
